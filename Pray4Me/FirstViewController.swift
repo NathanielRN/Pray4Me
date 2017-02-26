ee0ef5c7f6e7c6cac4b494e8c1ee0435cfee25bb
Nathaniel Ruiz <nathanielruiz98@gmail.com>
2017-02-25 21:48:32 -0500

    Initial Commit

---
+++ Pray4Me/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pray4Me
+//
+//  Created by Nathaniel Ruiz on 2017-02-25.
+//  Copyright © 2017 Nathaniel Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Pray4Me/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Pray4Me
+//
+//  Created by Nathaniel Ruiz on 2017-02-25.
+//  Copyright © 2017 Nathaniel Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
