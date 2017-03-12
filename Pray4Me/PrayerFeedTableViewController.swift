96c6aa67d3fa8525325f0ea5c2ad1d8a8a2d4d9d
Nathaniel Ruiz <nathanielruiz98@gmail.com>
2017-03-11 23:30:10 -0500

    Added ability to delete rows

--- Pray4Me/PrayerFeedTableViewController.swift
+++ Pray4Me/PrayerFeedTableViewController.swift
@@ -75,12 +75,12 @@ class PrayerFeedTableViewController: UITableViewController, RequestDetailsDelega
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+			self.prayerRequests.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    
+        }
+	}
 
     /*
     // Override to support rearranging the table view.
