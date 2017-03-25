74e29dc31fc1efbd9b6a760d05ae700a2292fac9
Nathaniel Ruiz <nathanielruiz98@gmail.com>
2017-03-25 11:07:38 -0400

    Adding file

---
+++ Pray4Me/EventsArrayModel.swift
@@ -0,0 +1,14 @@
+//
+//  EventsArrayModel.swift
+//  Pray4Me
+//
+//  Created by Nathaniel Ruiz on 2017-03-22.
+//  Copyright © 2017 Nathaniel Ruiz. All rights reserved.
+//
+
+import Foundation
+
+class EventsArrayModel {
+	
+	var eventsArray = [String]()
+}
