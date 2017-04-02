17af4727ef0b67ee96a0cf76b5668305c88fa0a7
Nathaniel Ruiz <nathanielruiz98@gmail.com>
2017-04-02 08:03:21 -0400

    Made main screen pop up automatically if logged in

--- Pray4Me/Pray4MeLoginViewController.swift
+++ Pray4Me/Pray4MeLoginViewController.swift
@@ -14,19 +14,31 @@ class Pray4MeLoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//		if (FBSDKAccessToken.current() != nil) {
+//			let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+//			let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "Startup") as UIViewController
+//			self.present(vc, animated: true, completion: nil)
+//		}
+
+//		let loginButton: FBSDKLoginButton = FBSDKLoginButton()
+//		loginButton.center = self.view.center
+//		self.view.addSubview(loginButton)
+//		loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+//		loginButton.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+	override func viewDidAppear(_ animated: Bool) {
+		if (FBSDKAccessToken.current() != nil) {
+			self.performSegue(withIdentifier: "LoggedInSegue", sender: nil)
+			return
+		}
 		let loginButton: FBSDKLoginButton = FBSDKLoginButton()
 		loginButton.center = self.view.center
 		self.view.addSubview(loginButton)
 		loginButton.readPermissions = ["public_profile", "email", "user_friends"]
 		loginButton.delegate = self
-
-		if (FBSDKAccessToken.current() != nil) {
-			let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-			let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "Startup") as UIViewController
-			self.present(vc, animated: true, completion: nil)
-		}
-        // Do any additional setup after loading the view.
-    }
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -36,9 +48,7 @@ class Pray4MeLoginViewController: UIViewController, FBSDKLoginButtonDelegate {
 	func loginButton(_ didCompleteWithloginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!){
 
 		if error == nil {
-			let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-			let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "Startup") as UIViewController
-			self.present(vc, animated: true, completion: nil)
+			self.performSegue(withIdentifier: "LoggedInSegue", sender: nil)
 		}
 	}
 	func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!){
